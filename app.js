9ed659bb4a5e5b0d0d8af7d0c8a397d22669e234
Paul Merrill <pdm@pdm.me>
2020-08-21 00:26:25 -0700

    starting to load spriteSheet

--- app.js
+++ app.js
@@ -1,12 +1,66 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
-var bg = new Image;
+loadImages(
+    {
+        bg: 'Original game assets/Layout.bmp',
+        spriteSheet: 'Original game assets/SPRITES.bmp',
+    },
+    function(results) {
+        // Draw results.bg 
+        ctx.drawImage(results.bg, 0, 0);
 
-// onload is older 'load' handler. It can only hold one event so it's not used as often anymore.
-// bg.onload = function() { ctx.drawImage(bg, 0, 0); };
-bg.addEventListener('load', function() {
-	ctx.drawImage(bg, 0, 0);
-});
-bg.src = 'Original game assets/Layout.bmp';
+        // Clip from spritesheet
+        ctx.rect(30, 30, 40, 50);
+        ctx.clip();
+        ctx.drawImage(results.spriteSheet, -50, -500);
+    }
+);
 
+/**
+* Load a set of images all at once.
+*
+* @param {object} images - A dictionary. Key = image name. Value = URL.
+* @param {function} onDone - function to be called when loading is done
+*                            it will be called with the value of "results"
+*/
+function loadImages(images, onDone) {
+   // Dictionary from image name to Image.
+   var results = {};
+
+   var doneLoadCount = 0;
+   var totalToLoad = 0;
+
+   for (var name in images) {
+       totalToLoad += 1;
+   }
+
+   function makeImage(name, src) {
+       var img = new Image;
+
+       img.addEventListener('load', function() {
+           results[name] = img;
+
+           doneLoadCount += 1;
+
+           if (doneLoadCount === totalToLoad) {
+               onDone(results);
+           }
+       });
+
+       img.addEventListener('error', function(error) {
+           console.error('Error loading image', src, error);
+       });
+
+       img.src = src;
+   }
+
+   for (var name in images) {
+       var src = images[name];
+       makeImage(name, src);
+   }
+}
+
+function clipCards(spriteSheet, xClip, yClip) {
+    
+}
